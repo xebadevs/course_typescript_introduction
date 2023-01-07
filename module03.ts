70cd1e52b72d7f21b1ac9b190a0a2e79fb0dc126
xebadevs <xebadevs@gmail.com>
2023-01-06 22:45:45 -0300

    module 03 exercise last changes

--- module03.ts
+++ module03.ts
@@ -12,7 +12,7 @@
 /*  TODO: Update the calculateInterestOnlyLoanPayment function. */
 
 interface Loan {
-    principal: number,
+    principle: number,
     interestRate: number
 }
 
@@ -24,22 +24,22 @@ function calculateInterestOnlyLoanPayment(loanTerms: Loan): string {
     // Calculates the monthly payment of an interest only loan
     let interest: number = loanTerms.interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
     let payment: number;
-    payment = loanTerms.principal * interest;
+    payment = loanTerms.principle * interest;
     return 'The interest only loan payment is ' + payment.toFixed(2);
 }
 
 /*  TODO: Update the calculateConventionalLoanPayment function. */
 
-function calculateConventionalLoanPayment(loanTerms: ConventionalLoan) {
+function calculateConventionalLoanPayment(loanTerms: ConventionalLoan): string {
     // Calculates the monthly payment of a conventional loan
     let interest: number = loanTerms.interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
     let payment: number;
-    payment = loanTerms.principal * interest / (1 - (Math.pow(1 / (1 + interest), loanTerms.months)));
+    payment = loanTerms.principle * interest / (1 - (Math.pow(1 / (1 + interest), loanTerms.months)));
     return 'The conventional loan payment is ' + payment.toFixed(2);
 }
 
-let interestOnlyPayment = calculateInterestOnlyLoanPayment({principal: 30000, interestRate: 5});
-let conventionalPayment = calculateConventionalLoanPayment({principal: 30000, interestRate: 5, months: 180});
+let interestOnlyPayment = calculateInterestOnlyLoanPayment({principle: 30000, interestRate: 5});
+let conventionalPayment = calculateConventionalLoanPayment({principle: 30000, interestRate: 5, months: 180});
 
 console.log(interestOnlyPayment);     //* Returns "The interest only loan payment is 125.00" 
 console.log(conventionalPayment);     //* Returns "The conventional loan payment is 237.24" 
