9bc74967d1c566df4fe1a175d712d423d7aae268
xebadevs <xebadevs@gmail.com>
2023-01-24 23:12:29 -0300

    build module 04

--- build/module04.js
+++ build/module04.js
@@ -1,13 +1,7 @@
 "use strict";
 /* Module 4: Develop typed functions using TypeScript
    Lab Start  */
-/*  EXERCISE 1
-    TODO: Declare a new function type for the sortDescending and sortAscending functions. */
-/*  TODO: Convert the sortDescending and sortAscending functions to arrow
-    functions. */
-/*  sortDescending is a comparison function that tells the sort method how to sort
-    numbers in descending order */
-function sortDescending(a, b) {
+let sortDescending = (a, b) => {
     if (a > b) {
         return -1;
     }
@@ -17,10 +11,10 @@ function sortDescending(a, b) {
     else {
         return 0;
     }
-}
+};
 /*  sortDescending is a comparison function that tells the sort method how to sort
     numbers in ascending order. */
-function sortAscending(a, b) {
+let sortAscending = (a, b) => {
     if (a > b) {
         return 1;
     }
@@ -30,11 +24,7 @@ function sortAscending(a, b) {
     else {
         return 0;
     }
-}
-/*  The buildArray function builds an array of unique random numbers containing the number
-    of items based on the number passed to it. The sortOrder parameter determines
-    whether to sort the array in ascending or descending order. */
-/*  TODO: Update the BuildArray function. */
+};
 function buildArray(items, sortOrder) {
     let randomNumbers = [];
     let nextNumber;
@@ -58,9 +48,9 @@ let myArray1 = buildArray(12, 'ascending');
 let myArray2 = buildArray(8, 'descending');
 /*  EXERCISE 2
     TODO: Update the LoanCalculator function. */
-function loanCalculator(principle, interestRate, months) {
+let loanCalculator = (principle, interestRate, months = 12) => {
     let interest = interestRate / 1200; // Calculates the monthly interest rate
     let payment;
     payment = principle * interest / (1 - (Math.pow(1 / (1 + interest), months)));
     return payment.toFixed(2);
-}
+};
